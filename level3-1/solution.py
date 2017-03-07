7fa0449b4e10112b62497d0d920cfe1e0a2b8fa9
顏孜羲 <joseph.yen@gmail.com>
2017-03-07 23:15:52 +0800

    level3-1 second
    cc:
    Asana:
    by: 顏孜羲 <joseph.yen@gmail.com>

--- level3-1/solution.py
+++ level3-1/solution.py
@@ -14,50 +14,22 @@
 #     (string) "4"
 
 
+from itertools import count
+
+
 def answer(M, F):
-    target_M, target_F = int(M), int(F)
+    S, L = sorted((int(M), int(F)))
+    step = 0
+    for step in count(0):
+        if S == L == 1:
+            return str(step)
 
-    steps = []
-    MFs = [(1, 1)]
-    next_step = 1
+        if S == 1:
+            return str(L - 1 + step)
 
-    def step_in(step):
-        if step == 1:
-            M, F = rep1(*MFs[-1])
-        elif step == 2:
-            M, F = rep2(*MFs[-1])
+        remainder = L % S
 
-        MFs.append((M, F))
-        steps.append(step)
-
-    while True:
-        print steps
-        print MFs
-        print next_step
-        print
-
-        M, F = MFs[-1]
-
-        if M == target_M and F == target_F:
-            return str(len(steps))
-
-        elif next_step == 3 and len(steps) == 0:
+        if remainder == 0:
             return 'impossible'
-
-        elif M > target_M or F > target_F or next_step == 3:
-            last_step = steps.pop()
-            MFs.pop()
-            next_step = (last_step + 1)
-
         else:
-            step_in(next_step)
-            next_step = 1
-
-
-
-def rep1(M, F):
-    return M, F+M
-
-
-def rep2(M, F):
-    return M+F, F
+            S, L = remainder, S
